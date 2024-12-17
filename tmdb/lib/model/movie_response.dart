337ef01db78cbd1a512307b763b647a6284eed7a
luisa <meron.camar24@triana.salesianos.edu>
2024-12-17 12:43:32 +0100

    clase 17/12

---
+++ tmdb/lib/model/movie_response.dart
@@ -0,0 +1,33 @@
+import 'package:tmdb/model/movie.dart';
+
+class MovieResponse {
+  int? page;
+  List<Movie>? results;
+  int? totalPages;
+  int? totalResults;
+
+  MovieResponse({this.page, this.results, this.totalPages, this.totalResults});
+
+  MovieResponse.fromJson(Map<String, dynamic> json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = <Movie>[];
+      json['results'].forEach((v) {
+        results!.add(Movie.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['page'] = page;
+    if (results != null) {
+      data['results'] = results!.map((v) => v.toJson()).toList();
+    }
+    data['total_pages'] = totalPages;
+    data['total_results'] = totalResults;
+    return data;
+  }
+}
